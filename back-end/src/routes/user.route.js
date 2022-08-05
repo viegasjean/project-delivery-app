0e42e7419520f692bc0db42d7b20d91ee85ce9ca
Breno Santos <brenosantos145@gmail.com>
2022-08-05 09:23:58 -0300

    Feat: Create user register endpoint

--- back-end/src/routes/user.route.js
+++ back-end/src/routes/user.route.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const controller = require('../controllers/user.controller');
 const bodyValidator = require('../middlewares/bodyValidator.middleware');
-const { loginSchema } = require('../schemas/user.schema');
+const { loginSchema, registerSchema } = require('../schemas/user.schema');
 
 const routes = Router();
 
 routes.post('/login', bodyValidator(loginSchema), controller.login);
+routes.post('/register', bodyValidator(registerSchema), controller.create);
 
 module.exports = routes;
--- back-end/src/schemas/user.schema.js
+++ back-end/src/schemas/user.schema.js
@@ -5,6 +5,14 @@ const loginSchema = joi.object({
   password: joi.string().min(8).required(),
 });
 
+const registerSchema = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().min(8).required(),
+  name: joi.string().min(3).required(),
+  role: joi.string().valid('customer', 'administrator', 'seller').required(),
+});
+
 module.exports = {
   loginSchema,
+  registerSchema,
 };
