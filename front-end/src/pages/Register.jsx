5bfc107e085ca7aa29949af4f07283b3e81afda3
unknown <davivarejao@gmail.com>
2022-08-05 21:05:33 -0300

    Feat: login and register pages

---
+++ front-end/src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RegisterForm from '../components/RegisterForm';
+
+function Register() {
+  return (
+    <section>
+      <h1>Cadastro</h1>
+      <RegisterForm />
+    </section>
+  );
+}
+
+export default Register;
