f9375edf8abe94df47064866cebeea2186402cb5
Breno Santos <brenosantos145@gmail.com>
2022-08-09 11:40:28 -0300

    :sparkles: Fix: lint

--- back-end/src/controllers/user.controller.js
+++ back-end/src/controllers/user.controller.js
@@ -4,7 +4,6 @@ const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const response = await service.login({ email, password });
-    console.log(req.body);
     return res.status(200).json({ ...response });
   } catch (error) {
     next(error);
@@ -14,13 +13,9 @@ const login = async (req, res, next) => {
 const create = async (req, res, next) => {
   try {
     const { email, password, name, role } = req.body;
-    await service.create({ email, password, name, role });
+    const response = await service.create({ email, password, name, role });
 
-<<<<<<< HEAD
-    return res.status(201).json({ message: 'User created successfully' });
-=======
     return res.status(201).json({ ...response });
->>>>>>> 058cacd (:sparkles: Feat: login page)
   } catch (error) {
     next(error);
   }
