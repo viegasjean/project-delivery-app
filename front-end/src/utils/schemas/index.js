b3a91c411ee968edeed55fb6c11d62e019cab406
unknown <davivarejao@gmail.com>
2022-08-09 16:07:22 -0300

    Feat: add registerSchema

--- front-end/src/utils/schemas/index.js
+++ front-end/src/utils/schemas/index.js
@@ -1,19 +1,20 @@
 const yup = require('yup');
 
 const minPassword = 6;
-// const fullName = 12;
+const fullName = 12;
 
 const loginSchema = yup.object().shape({
   email: yup.string().email().required(),
   password: yup.string().min(minPassword).required(),
 });
 
-// const registerSchema = yup.object().shape({
-//   name: yup.string().min(fullName).required(),
-//   email: yup.string().email().required(),
-//   password: yup.string().min(password).required(),
-// });
+const registerSchema = yup.object().shape({
+  name: yup.string().min(fullName).required(),
+  email: yup.string().email().required(),
+  password: yup.string().min(minPassword).required(),
+});
 
 module.exports = {
   loginSchema,
+  registerSchema,
 };
