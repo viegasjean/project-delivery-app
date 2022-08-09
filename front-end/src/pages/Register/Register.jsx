403e909740e30175d3ae823a71bd5fcf59e2dfe3
unknown <davivarejao@gmail.com>
2022-08-09 16:08:43 -0300

    Feat: create new Register screen

--- front-end/src/pages/Register/Register.jsx
+++ front-end/src/pages/Register/Register.jsx
@@ -1,10 +1,113 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import style from '../Login/style.module.css';
+
+import { registerSchema } from '../../utils/schemas';
+import { register } from '../../services/api';
+import { created } from '../../services/constants';
+import { setKey } from '../../services/localStorage';
 
 function Register() {
+  const [data, setData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [error, setError] = useState('');
+  const history = useHistory();
+
+  const handleChange = ({ target: { value, name } }) => {
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    const { name, email, password } = data;
+    registerSchema.isValid({
+      name,
+      email,
+      password,
+    }).then((validation) => {
+      if (validation) setIsDisabled(false);
+      else setIsDisabled(true);
+    });
+  }, [data]);
+
+  const submit = async (e) => {
+    e.preventDefault();
+    const res = await register(data);
+    if (res.status === created) {
+      setKey('deliveryUser', res.data);
+      history.push('/customer/products');
+    } else {
+      setError(res.data);
+    }
+  };
+
+  const toLogin = () => {
+    history.push('/');
+  };
+
   return (
-    <section>
+    <div className={ style.container }>
       <h1>Cadastro</h1>
-    </section>
+      <form>
+        <label htmlFor="name">
+          Nome:
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={ handleChange }
+            data-testid="common_register__input-name"
+          />
+        </label>
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={ handleChange }
+            data-testid="common_register__input-email"
+          />
+        </label>
+        <label htmlFor="password">
+          Senha:
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={ handleChange }
+            data-testid="common_register__input-password"
+          />
+        </label>
+        <button
+          type="button"
+          onClick={ submit }
+          disabled={ isDisabled }
+          data-testid="common_register__button-register"
+        >
+          Cadastrar
+        </button>
+        <button
+          type="button"
+          onClick={ toLogin }
+        >
+          Voltar para Login
+        </button>
+      </form>
+      { error && (
+        <span
+          data-testid="common_register__element-invalid_register"
+        >
+          {error.message}
+        </span>
+      )}
+    </div>
   );
 }
 
