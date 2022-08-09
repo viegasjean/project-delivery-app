d60b802c28a2cea1e14aeeb16c843b5eaf720d88
unknown <davivarejao@gmail.com>
2022-08-09 16:08:15 -0300

    Feat: add register request

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -13,6 +13,16 @@ const login = async (user) => {
 
 // login({email: "bola8@gmail.com", password: "senhaBonita"})
 
+const register = async (user) => {
+  try {
+    const { status, data } = await axios.post(`${url}/user/register`, user);
+    return { status, data };
+  } catch ({ response: { status, data } }) {
+    return { status, data };
+  }
+};
+
 module.exports = {
   login,
+  register,
 };
