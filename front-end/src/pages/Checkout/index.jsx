c956ec2ac0ee0cd5c736da897a2dde034f21741f
Jean Viegas <jeanmelg@gmail.com.br>
2022-08-11 12:52:28 -0300

    feat: save salesProducts on click in "finalziar pedido"

--- front-end/src/pages/Checkout/index.jsx
+++ front-end/src/pages/Checkout/index.jsx
@@ -1,16 +1,63 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { CartContext } from '../../context/cart';
+import { getUsers, saveSale, saveSalesProducts } from '../../services/api';
+import { getKey } from '../../services/localStorage';
 
 export default function Checkout() {
   const { cart, removeItemFromCart } = useContext(CartContext);
-  const [total, setTotal] = useState(0);
+  // const [total, setTotal] = useState(0);
+  const [sellers, setSellers] = useState([]);
+  const [sale, setSale] = useState({
+    sellerId: '',
+    userId: getKey('deliveryUser').id,
+    totalPrice: 0,
+    deliveryAddress: '',
+    deliveryNumber: '',
+    saleDate: new Date(),
+    status: 'Pendente',
+  });
+  const history = useHistory();
 
   useEffect(() => {
-    setTotal(cart.reduce((acc, cartItem) => {
-      acc += cartItem.subTotal;
-      return acc;
-    }, 0));
-  });
+    getUsers('seller').then(({ data }) => { setSellers(data); });
+  }, []);
+
+  useEffect(() => {
+    setSale({
+      ...sale,
+      totalPrice: cart.reduce((acc, cartItem) => {
+        acc += cartItem.subTotal;
+        return acc;
+      }, 0),
+    });
+  }, [cart]);
+
+  const handleChange = ({ target: { value, name } }) => {
+    setSale({
+      ...sale,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = () => {
+    const { token } = getKey('deliveryUser');
+    saveSale(sale, token)
+      .then(({ data }) => {
+        console.log(data);
+
+        const salesProducts = cart.filter((item) => item.quantity > 0)
+          .map((item) => ({
+            saleId: data.id, productId: item.id, quantity: item.quantity,
+          }));
+
+        console.log('salesProducts', salesProducts);
+
+        saveSalesProducts(salesProducts, token);
+
+        history.push(`/customer/orders/${data.id}`);
+      });
+  };
 
   return (
     <>
@@ -71,26 +118,42 @@ export default function Checkout() {
         <h2
           data-testid="customer_checkout__element-order-total-price"
         >
-          { total.toFixed(2).replace(/\./, ',') }
+          { sale.totalPrice.toFixed(2).replace(/\./, ',') }
         </h2>
       </section>
       <section>
         <select
           data-testid="customer_checkout__select-seller"
+          name="sellerId"
+          onChange={ handleChange }
+          onClick={ handleChange }
         >
-          <option>VENDEDOR</option>
+          {sellers && sellers.map((seller) => (
+            <option
+              key={ seller.id }
+              value={ seller.id }
+            >
+              {seller.name}
+            </option>
+          ))}
+
         </select>
         <input
           data-testid="customer_checkout__input-address "
           type="text"
+          name="deliveryAddress"
+          onChange={ handleChange }
         />
         <input
           data-testid="customer_checkout__input-addressNumber"
           type="text"
+          name="deliveryNumber"
+          onChange={ handleChange }
         />
         <button
           type="button"
           data-testid="customer_checkout__button-submit-order"
+          onClick={ handleSubmit }
         >
           FINALIZAR PEDIDO
         </button>
