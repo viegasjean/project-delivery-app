f050d4a232b10334edd5a07b8b6f2ca7bf5f5ef7
Lucas Ferreira <lucas6rodrigues@yahoo.com.br>
2022-08-12 17:38:22 -0300

    feat(backend/routes) routes for saleByUserId and list all sales

--- back-end/src/routes/sale.route.js
+++ back-end/src/routes/sale.route.js
@@ -3,6 +3,9 @@ const controller = require('../controllers/sale.controller');
 
 const routes = Router();
 
+routes.get('/', controller.list);
+routes.get('/:id', controller.saleByUserId);
+
 routes.post('/save', controller.create);
 
 module.exports = routes;
