ad354d04a01d502e06ccfaac74edd6c0857023b3
Lucas Ferreira <lucas6rodrigues@yahoo.com.br>
2022-08-15 14:26:00 -0300

    lint

--- back-end/src/services/sale.service.js
+++ back-end/src/services/sale.service.js
@@ -8,15 +8,15 @@ const create = async (sale) => {
 const list = async () => {
   const sales = await model.findAll();
   return sales;
-}
+};
 
 const salesByUserId = async (userId) => {
   const found = await model.findAll({ where: { userId } });
   return found;
-}
+};
 
 module.exports = {
   create,
   list,
-  salesByUserId
+  salesByUserId,
 };
